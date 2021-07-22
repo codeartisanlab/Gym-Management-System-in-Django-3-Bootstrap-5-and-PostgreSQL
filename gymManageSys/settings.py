2bae93036e5ba9a5447743364f8b08c4054a496a
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-07-22 09:39:29 +0530

    part-7-change-django-admin-ui-with-django-jazzmin-package

--- gymManageSys/settings.py
+++ gymManageSys/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS={
+    "site_title": "GMS Admin",
+    "site_header": "GMS Admin",
+    "order_with_respect_to": ["auth", "main.banners","main.service","main.enquiry","main.gallery","main.GalleryImage","main.Page"],
+}
